a89cbfd5fe2911fcca5f038ec15a375b48b117ff
Shuhao Zhang <eclipse.tenet@gmail.com>
2023-05-22 21:08:35 +0800

    Update msgs inside deploy and function test code.

--- src/deploy.sh
+++ src/deploy.sh
@@ -10,4 +10,11 @@ curl -sfL https://direnv.net/install.sh | bash
 echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
 direnv allow
 make build
-# Failed. Error unknown.
+# Failed. How to cause error is unknown for development group.
+# Error Message:
+# cd src && gcc template.bpf.c template.htemplate.bpf.c:3:10: fatal error: vmlinux.h: No such file or directory
+# 3#include "vmlinux .h"
+# >2222222222compilation terminated.template.h:16:9: error: unknown type name
+# bool exit_event;16
+# >222
+# make: x** [Makefile:2: build] Error 1
--- src/function.sh
+++ src/function.sh
@@ -4,3 +4,9 @@
 # Author: zhsh.
 # Environment: Debian-based(or Ubuntu-based), apt.
 
+# How to use built file to achieve project gaol.
+# 1. Run the binary file.
+ecli run src/package.json
+
+# 2. Run with Github Packages locally.
+# docker run --rm -it --privileged -v $(pwd):/examples ghcr.io/eunomia-bpf/eunomia-template:latest
